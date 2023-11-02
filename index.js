a78874ff2a57671999d96053ea804b4620a0c0b5
Luke <ldv432@gmail.com>
2023-11-01 22:18:21 -0600

    math

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
-// Write your code here
+const num1 = 62
+const num2 = 1
+const multiply = num1 * num2
+const random = Math.ceil(Math.random() * 10) + 1;
+const num3 = 22
+const num4 = 9
+const mod = num3 % num4
+const nums = (0, 5, 10, 20)
+const max = Math.max(nums)
